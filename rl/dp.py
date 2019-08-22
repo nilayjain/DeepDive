277bf3de584c32c3af2ea9b0b53ce50e1962ff71
nilayjain <nilayjain13@gmail.com>
2019-08-22 10:19:36 +0530

    added policy iteration algorithm

--- rl/dp.py
+++ rl/dp.py
@@ -11,6 +11,11 @@ class DynamicProgramming:
         self.theta = 0.00001
 
     def policy_evaluation(self, policy) -> np.ndarray:
+        """
+        Evaluate a policy and return its value function.
+        :param policy: policy to be evaluated.
+        :return: value function v_pi
+        """
         V: np.ndarray = np.zeros(self.env.nS)
         while True:
             delta: float = 0.
@@ -26,7 +31,39 @@ class DynamicProgramming:
         return V
 
     def policy_iteration(self):
-        pass
+        """
+        Start with a random policy. Perform Generalized Policy Iteration (GPI)
+        to find the optimal policy and the optimal value function.
+        GPI: do policy evaluation followed by policy improvement (act greedily
+        w.r.t the value function).
+        :return: optimal policy pi_*, and value function V_*
+        """
+        policy = np.ones(self.env.nS, self.env.nA) / 4.
+        while True:
+            policy_stable: bool = True
+            V: np.ndarray = self.policy_evaluation(policy)
+            new_policy = np.zeros_like(policy)
+            for s in range(self.env.nS):
+                old_action: int = np.argmax(policy[s])
+                action_values = self._one_step_lookahead(s, V)
+                new_action = np.argmax(action_values)
+                new_policy[new_action] = 1
+                if old_action != new_action:
+                    policy_stable = False
+            if policy_stable:
+                return policy, V
 
     def value_iteration(self):
         pass
+
+    def _one_step_lookahead(self, s: int, V: np.ndarray) -> np.ndarray:
+        """
+        Calculate all action values from state s.
+        :param s: state
+        :return: action values.
+        """
+        av = np.zeros(self.env.nA)
+        for a in range(self.env.nA):
+            for p, ns, r, _ in self.env.P[s][a]:
+                av[a] += p * (r + self.gamma * V[ns])
+        return av
