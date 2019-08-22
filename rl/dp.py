311712a88a5bec802ac1f52c030890dd6b43c8f0
nilayjain <nilayjain13@gmail.com>
2019-08-22 09:53:47 +0530

    added policy evaluation method

--- rl/dp.py
+++ rl/dp.py
@@ -1,11 +1,32 @@
+import numpy as np
+
+from rl.envs.gridworld import GridWorld
+
+
 class DynamicProgramming:
 
-    def policy_eval(self, policy, env, discount_factor=1.0, theta=0.00001):
+    def __init__(self):
+        self.env = GridWorld()
+        self.gamma = 1.
+        self.theta = 0.00001
+
+    def policy_evaluation(self, policy) -> np.ndarray:
+        V: np.ndarray = np.zeros(self.env.nS)
+        while True:
+            delta: float = 0.
+            for s in range(self.env.nS):
+                v: float = 0.
+                for a in range(self.env.nA):
+                    for p, ns, reward, done in self.env.P[s][a]:
+                        v += policy[s, a] * (reward + p * self.gamma * V[ns])
+                delta = max(delta, abs(v - V[s]))
+                V[s] = v
+            if delta < self.theta:
+                break
+        return V
+
+    def policy_iteration(self):
         pass
 
-    def policy_iteration(self, env, policy_eval_fn=policy_eval,
-                         discount_factor=1.0):
-        pass
-
-    def value_iteration(self, env, theta=0.0001, discount_factor=1.0):
+    def value_iteration(self):
         pass
